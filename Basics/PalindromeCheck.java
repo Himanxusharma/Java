cc598ec858a21113f57d631c674cb26b6dc0309a
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-02-14 13:00:47 +0530

    14-02-2023

---
+++ Basics/PalindromeCheck.java
@@ -0,0 +1,36 @@
+import java.lang.String;
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+        
+        String revStr = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string : ");
+        String str = sc.nextLine();
+
+        // One Method 
+        // for(var i = str.length() - 1 ; i >= 0; i--)  //reverse looop
+        //     {
+        //         revStr += str.charAt(i); 
+        //     }
+
+        // if(str.equalsIgnoreCase(revStr)) System.out.println("Yes, This String is palindrome. ");
+        // else System.out.println("No, This String is not palindrome. ");
+
+
+        //Optimized Solution
+        
+        Boolean isPalin = false;
+        for(int i=0 ; i < str.length() / 2 ; i++) {
+            if(str.charAt(i)==str.charAt(str.length()-i-1)) isPalin = true;
+
+        } 
+        System.out.println("Is this String Palindrome , Condition : " + isPalin);
+
+
+
+    }
+    
+}
