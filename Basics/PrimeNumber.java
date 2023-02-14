3246f092864fff9c1fb91437810e623960213e25
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-02-14 16:59:13 +0530

    14-02-2023

--- Basics/PrimeNumber.java
+++ Basics/PrimeNumber.java
@@ -1,26 +1,22 @@
-import java.lang.String;
 import java.util.Scanner;
 import java.lang.Math;
 
 public class PrimeNumber {
     
     public static void main(String[] args) {
-        
-    
+  
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the num : ");
     int n = sc.nextInt();
     Boolean isPrime= true;
 
-    for(int i = 2; i <= Math.sqrt(n) ; i++)
+    for(int i = 2; i <= Math.sqrt(n) ; i++) //Math.sqrt() from the math lib to find square root
     {
         if(n==1) isPrime=true;
         if(n%i==0) isPrime=false;
     }
 
     System.out.println("Is " + n + " prime , Condition : " + isPrime);
-
-
     }
 
 }
