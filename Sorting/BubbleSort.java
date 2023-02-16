5dcf6f19293977618c111a604939818da49e0c44
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-02-16 16:52:27 +0530

    16-02-23

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,10 +1,6 @@
 package Sorting;
 import java.util.*;
 public class BubbleSort {
-    
-
-
-
     static void BubbleS(int[] arr)
         {
             int temp;
@@ -34,7 +30,7 @@ public class BubbleSort {
         public static void main(String[] args) {
  
         Scanner sc = new Scanner(System.in);
-        System.out.print(" Enter the number of element : ");
+        System.out.print("Enter the number of element : ");
         int n = sc.nextInt();
         int [] arr = new int [n];
         for (int i = 0; i < n; i++) {
