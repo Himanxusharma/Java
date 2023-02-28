76b0c89fddcc2774c771fcee2e9e41d66b6174da
Himanshu Sharma <himanshugaurbsr@gmail.com>
2023-02-28 15:28:36 +0530

    #

--- Threading/InterruptDemo.java
+++ Threading/InterruptDemo.java
@@ -9,7 +9,10 @@ public class InterruptDemo extends Thread {
             
             for (int i = 1; i < 6; i++) {
                 System.out.println(i);
+                //System.out.println(isInterrupted());
+                System.out.println(Thread.interrupted()); //true and change to trur to false.
                 Thread.sleep(1000);
+                System.out.println(Thread.currentThread().isInterrupted());
             }
 
 
